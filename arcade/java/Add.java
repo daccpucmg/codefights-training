1f2e85fc83cfdfe01f1753ef6b78e23e3a983c30
daccpucmg <32392890+daccpucmg@users.noreply.github.com>
2017-10-11 21:44:00 -0300

    Create Add.java
    https://codefights.com/arcade/intro/level-1/jwr339Kq6e3LQTsfa

---
+++ arcade/java/Add.java
@@ -0,0 +1,12 @@
+public class Add {
+  
+  // O metodo abaixo resolve a questao Add (https://codefights.com/arcade/intro/level-1/jwr339Kq6e3LQTsfa) em Java  
+  public static int add(int param1, int param2) {
+    return param1 + param2;
+  }
+  
+  public static void main(String[] args) {
+    sum = add(5, 3);
+    System.out.println(sum);
+  }
+}
