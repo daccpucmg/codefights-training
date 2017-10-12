8a8b16c8b8c6532eeb9bd106bdbcc6299db010d6
daccpucmg <32392890+daccpucmg@users.noreply.github.com>
2017-10-11 21:58:51 -0300

    Create add.c
    https://codefights.com/arcade/intro/level-1/jwr339Kq6e3LQTsfa

---
+++ arcade/c/add.c
@@ -0,0 +1,11 @@
+int main() {
+  // O metodo abaixo resolve a questao Add (https://codefights.com/arcade/intro/level-1/jwr339Kq6e3LQTsfa) em C 
+  int add(int param1, int param2) {
+      return param1 + param2;
+  }
+
+  int soma = add(5,3);
+  cout << soma;
+  
+  return 0;
+}
