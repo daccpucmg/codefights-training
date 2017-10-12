471c23c9f3462dc07c2090a08ce4013efa37dbb0
daccpucmg <32392890+daccpucmg@users.noreply.github.com>
2017-10-11 22:06:06 -0300

    Create add.go
    https://codefights.com/arcade/intro/level-1/jwr339Kq6e3LQTsfa

---
+++ arcade/go/add.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// O metodo abaixo resolve a questao Add (https://codefights.com/arcade/intro/level-1/jwr339Kq6e3LQTsfa) em Java
+func add(param1 int, param2 int) int {
+    return param1 + param2
+}
+
+func main() {
+    fmt.Println(add(5, 3))
+}
