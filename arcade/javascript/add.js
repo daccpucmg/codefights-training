8dee130a4117483b9225133e32810b46b247db96
daccpucmg <32392890+daccpucmg@users.noreply.github.com>
2017-10-11 22:01:10 -0300

    Create add.js
    https://codefights.com/arcade/intro/level-1/jwr339Kq6e3LQTsfa

---
+++ arcade/javascript/add.js
@@ -0,0 +1,6 @@
+// O metodo abaixo resolve a questao Add (https://codefights.com/arcade/intro/level-1/jwr339Kq6e3LQTsfa) em JavaScript
+function add(param1, param2) {
+    return param1 + param2;
+}
+
+add(5,3);
