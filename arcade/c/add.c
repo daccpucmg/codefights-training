c592c255b5fffbb44d4763a557dd8c2f8bae6ed8
daccpucmg <32392890+daccpucmg@users.noreply.github.com>
2017-10-18 00:40:26 -0200

    Revert "Fix add.c - close issue #2"

--- arcade/c/add.c
+++ arcade/c/add.c
@@ -1,13 +1,11 @@
-// O metodo abaixo resolve a questao Add (https://codefights.com/arcade/intro/level-1/jwr339Kq6e3LQTsfa) em C 
-int add(int param1, int param2) {
-	return param1 + param2;
-}
-
 int main() {
+  // O metodo abaixo resolve a questao Add (https://codefights.com/arcade/intro/level-1/jwr339Kq6e3LQTsfa) em C 
+  int add(int param1, int param2) {
+      return param1 + param2;
+  }
 
   int soma = add(5,3);
-  printf("%d", soma);
+  cout << soma;
   
   return 0;
-
 }
