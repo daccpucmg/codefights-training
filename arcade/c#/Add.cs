bb57a391339f269b13b2ce82e1f4efc874e5754e
Geyson Jr <geyson.junior@sga.pucminas.br>
2017-10-17 22:07:27 -0200

    Create Add c#

---
+++ arcade/c#/Add.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Arcade
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(add(1,2));
+        }
+
+        private static void add(int i, int j)
+        {
+            return i + j;
+        }
+    }
+}
