04bf796914027cb3ab92c12098e00dfd56fe7a8c
Gabriella Mara <gabriellamara.p@gmail.com>
2017-10-28 21:44:44 -0200

    Creating ShapeArea in Haskell

---
+++ arcade/haskell/shapeArea.hs
@@ -0,0 +1,9 @@
+{-
+	
+	Resolução do exercício ShapeArea (https://codefights.com/arcade/intro/level-2/yuGuHvcCaFCKk56rJ) em Haskell
+	@author Gabriella Mara
+	@version 1.0
+	
+	-}
+
+shapeArea n = n ^ 2 + (n - 1) ^ 2
